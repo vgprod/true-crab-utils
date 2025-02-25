975513934a75714680ea2f4990000d747aae427e
Vladan Gojgic <v.gojgic@gmail.com>
2025-02-25 19:30:59 +0100

    Refactor package name and add initial algorithm and data structure modules

---
+++ src/algorithms/search.rs
@@ -0,0 +1,11 @@
+/// Performs binary search on a sorted slice.
+///
+/// # Examples
+///
+/// ```
+/// use my_algorithms::algorithms::search::binary_search;
+///
+///
+pub fn binary_search() {
+    todo!()
+}
